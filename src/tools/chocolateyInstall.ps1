5d174419924f391c5df0c3caba436473d9cd2937
Chih-Hsuan Yen <645432-yan12125@users.noreply.gitlab.com>
2024-06-24 20:19:34 +0800

    Fix installation on non-English systems
    For example, on a Traditional Chinese system, the installer has window
    title "OpenConnect-GUI 安裝", so the AutoHotkey script never sees a
    matching window and fails. Here I use the silent installation mode
    provided by the installer instead.

--- src/tools/chocolateyInstall.ps1
+++ src/tools/chocolateyInstall.ps1
@@ -4,25 +4,14 @@ $Global:ErrorActionPreference = 'Stop'
 $Global:InformationPreference = 'Continue'
 $Global:VerbosePreference     = 'Continue'
 
-$autoHotkeyArgs = @{
-  FilePath     = Get-ChocolateyPath -PathType InstallPath | Join-Path -ChildPath "lib\autohotkey.portable\tools\AutoHotkey.exe"
-  ArgumentList = Get-ChocolateyPath -PathType PackagePath | Join-Path -ChildPath "tools\install.ahk"
-  PassThru     = $true
-}
-
-$AutoHotKeyProcess = Start-Process @autoHotkeyArgs
-Write-Verbose "Running $($AutoHotKeyProcess.ProcessName)"
-
 
 $PackageArgs = @{
   PackageName  = 'openconnect-gui'
   FileType     = 'exe'
-  SilentArgs   = ''
+  SilentArgs   = '/S'
   Url          = "https://www.infradead.org/openconnect-gui/download/openconnect-gui-$($PackageVersion)-win64.exe"
   Checksum     = '8BD369BE0F5F4AB4C7E64E2204AE5162BEB86AD9AB7FC2752D67429170A18E4BE77A8F36667FF229BC6A444B2BF005BF6596708DEA4DC2B75DF2BDD1FC2E6BD0'
   ChecksumType = 'sha512'
 }
 
 Install-ChocolateyPackage @PackageArgs
-Wait-Process -InputObject $AutoHotKeyProcess -ErrorAction SilentlyContinue
-Write-Verbose "Process $($AutoHotKeyProcess.ProcessName): HasExited $($AutoHotKeyProcess.HasExited) ExitCode $($AutoHotKeyProcess.ExitCode)"
