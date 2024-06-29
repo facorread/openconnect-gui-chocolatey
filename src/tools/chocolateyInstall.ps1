24ae67061a4db59ffa2987e647f2441fdf91b645
Dmitry Romashov <framebassman@gmail.com>
2024-06-29 14:19:48 +0200

    Code review

--- src/tools/chocolateyInstall.ps1
+++ src/tools/chocolateyInstall.ps1
@@ -19,7 +19,7 @@ $PackageArgs = @{
   FileType     = 'exe'
   SilentArgs   = ''
   Url          = "https://www.infradead.org/openconnect-gui/download/openconnect-gui-$($PackageVersion)-win64.exe"
-  Checksum     = 'e8f244c52e9a82f3a8d8d87cbdce95f900c65e931fca3569ad338b0e0ee17b83fc7a7873c3997e7e6af77d79ad6a1c06eaa6187a26126c5842552e1e0f570799  '
+  Checksum     = 'e8f244c52e9a82f3a8d8d87cbdce95f900c65e931fca3569ad338b0e0ee17b83fc7a7873c3997e7e6af77d79ad6a1c06eaa6187a26126c5842552e1e0f570799'
   ChecksumType = 'sha512'
 }
 
