d5e2db21bd96f90f1b395fb11f2c1b8ea6a27517
Fabio A. Correa <facorread@gmail.com>
2024-07-14 22:04:29 -0400

    1.6.2 sans AutoHotKey

--- src/tools/chocolateyInstall.ps1
+++ src/tools/chocolateyInstall.ps1
@@ -1,28 +1,16 @@
-﻿$PackageVersion = "1.6.1"
+﻿$PackageVersion = "1.6.2"
 
 $Global:ErrorActionPreference = 'Stop'
 $Global:InformationPreference = 'Continue'
 $Global:VerbosePreference     = 'Continue'
 
-$autoHotkeyArgs = @{
-  FilePath     = Get-ChocolateyPath -PathType InstallPath | Join-Path -ChildPath "lib\autohotkey.portable\tools\AutoHotkey.exe"
-  ArgumentList = Get-ChocolateyPath -PathType PackagePath | Join-Path -ChildPath "tools\install.ahk"
-  PassThru     = $true
-}
-
-$AutoHotKeyProcess = Start-Process @autoHotkeyArgs
-Write-Verbose "Running $($AutoHotKeyProcess.ProcessName)"
-
-
 $PackageArgs = @{
   PackageName  = 'openconnect-gui'
   FileType     = 'exe'
-  SilentArgs   = ''
+  SilentArgs   = '/S'
   Url          = "https://www.infradead.org/openconnect-gui/download/openconnect-gui-$($PackageVersion)-win64.exe"
-  Checksum     = '8BD369BE0F5F4AB4C7E64E2204AE5162BEB86AD9AB7FC2752D67429170A18E4BE77A8F36667FF229BC6A444B2BF005BF6596708DEA4DC2B75DF2BDD1FC2E6BD0'
+  Checksum     = 'e8f244c52e9a82f3a8d8d87cbdce95f900c65e931fca3569ad338b0e0ee17b83fc7a7873c3997e7e6af77d79ad6a1c06eaa6187a26126c5842552e1e0f570799'
   ChecksumType = 'sha512'
 }
 
 Install-ChocolateyPackage @PackageArgs
-Wait-Process -InputObject $AutoHotKeyProcess -ErrorAction SilentlyContinue
-Write-Verbose "Process $($AutoHotKeyProcess.ProcessName): HasExited $($AutoHotKeyProcess.HasExited) ExitCode $($AutoHotKeyProcess.ExitCode)"
